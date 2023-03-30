2805f82ca682de1a836cae6bef8484bf902ee261
Paulo Freitas <login.paulofreitas@hotmail.com>
2023-03-30 00:33:21 -0300

    new commit

---
+++ src/exercises/PositiveOrNegative.java
@@ -0,0 +1,23 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class PositiveOrNegative {
+
+    public static void main(String[] args) {
+
+        /* Make a program that asks for a value and shows
+       on the screen if the value is positive or negative! */
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a number:");
+        int number = scan.nextInt();
+
+        if (number >= 0) {
+            System.out.println("Positive Number!");
+        } else {
+            System.out.println("Negative Number");
+        }
+    }
+}
