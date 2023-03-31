196b82b4c43264be041cb3283955f3f116a30cd3
Paulo Freitas <login.paulofreitas@hotmail.com>
2023-03-31 01:43:20 -0300

    new commit

---
+++ src/exercises/Factorial.java
@@ -0,0 +1,24 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        // Write a program that calculates the factorial of a user-supplied integer!
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a number:");
+        int number = scan.nextInt();
+
+        int factorial = 1;
+
+        while (number >= 1) {
+            factorial *= number;
+            number--;
+        }
+        System.out.println("The factorial of the entered number is: " + factorial);
+    }
+}
