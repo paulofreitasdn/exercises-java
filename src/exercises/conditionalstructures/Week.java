7807262b89f4ea1f569ea8092fcbc63d5c499a5a
Paulo Freitas <login.paulofreitas@hotmail.com>
2023-05-27 00:12:44 -0300

    new commit

--- src/exercises/conditionalstructures/Week.java
+++ src/exercises/conditionalstructures/Week.java
@@ -13,31 +13,22 @@ public class Week {
         System.out.print("Enter a number: ");
         int number = scan.nextInt();
 
-        switch (number) {
-
-            case 1:
-                System.out.println("Sunday");
-                break;
-            case 2:
-                System.out.println("Monday");
-                break;
-            case 3:
-                System.out.println("Tuesday");
-                break;
-            case 4:
-                System.out.println("Wednesday");
-                break;
-            case 5:
-                System.out.println("Thursday");
-                break;
-            case 6:
-                System.out.println("Friday");
-                break;
-            case 7:
-                System.out.println("Saturday");
-                break;
-            default:
-                System.out.println("Invalid value!");
+        if (number == 1) {
+            System.out.println("Sunday");
+        } else if (number == 2) {
+            System.out.println("Monday");
+        } else if (number == 3) {
+            System.out.println("Tuesday");
+        } else if (number == 4) {
+            System.out.println("Wednesday");
+        } else if (number == 5) {
+            System.out.println("Thursday");
+        } else if (number == 6) {
+            System.out.println("Friday");
+        } else if (number == 7) {
+            System.out.println("Saturday");
+        } else {
+            System.out.println("Invalid value!");
         }
     }
 }
